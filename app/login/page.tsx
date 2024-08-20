62ffe406b3e7805649696257733b02ad2db0af15
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2024-08-20 18:33:32 -0400

    Added demo video and screenshots.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "https://codequest-app-eta.vercel.app/courses",
+        redirectTo: "/courses",
       },
     });
 
